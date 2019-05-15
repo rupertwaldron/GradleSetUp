ff9a592a23da43321447b5f533db08c3f54c7f48
rupertwaldron <rupert.waldron@yahoo.co.uk>
2019-05-15 21:18:40 +0100

    Set up gradle project

---
+++ src/main/java/com/ruppyrup/gradlelastchance/GradleLastChanceApplication.java
@@ -0,0 +1,13 @@
+package com.ruppyrup.gradlelastchance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleLastChanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleLastChanceApplication.class, args);
+	}
+
+}
